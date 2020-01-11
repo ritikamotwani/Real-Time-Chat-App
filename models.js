41286b5b3d27d2f41f8f980d9d98fda587fe4d47
ritikamotwani <ritika.motwani0712@gmail.com>
2020-01-11 12:00:32 +0530

    Backend: Add the models required(Users and Messages)

---
+++ models.js
@@ -0,0 +1,18 @@
+const User = mongoose.model("User", {
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        index: { unique: true }
+    }
+});
+
+const Message = mongoose.model("Message", {
+    message: String,
+    senderMail: String,
+    receiverMail: String,
+    timestamp: Number
+});
