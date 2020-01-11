1178afb86b578fa7542b4f74a9248d7bd49fab17
ritikamotwani <ritika.motwani0712@gmail.com>
2020-01-11 23:41:33 +0530

    Frontend: Import libs in index.js and wrap the app in Apollo Provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,44 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import { split } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import "./index.css";
+import App from "./App";
 
+const httpLink = new HttpLink({
+    uri: 'http://localhost:4000/'
+});
+
+const wsLink = new WebSocketLink({
+    uri: `ws://localhost:4000/`,
+    options: {
+        reconnect: true
+    }
+});
+
+const link = split(
+    ({ query }) => {
+        const { kind, operation } = getMainDefinition(query);
+        return kind === 'OperationDefinition' && operation === 'subscription';
+    },
+    wsLink,
+    httpLink,
+);
+// query mutation to httpLink, Subscription to wsLink
+
+const client = new ApolloClient({
+    link,
+    cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+<ApolloProvider client={client}>
+    <App />
+    </ApolloProvider>,
+document.getElementById("root")
+);
